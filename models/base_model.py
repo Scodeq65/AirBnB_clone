aa2be6b7180381b98078ab2ac2a61dbfdbba899e
Scodeq65 <sodiqagbaraojo@gmail.com>
2024-05-19 00:38:53 -0700

    storage_file

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@
 
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -20,14 +21,17 @@ class BaseModel:
         """Initializes a new instance of BaseModel."""
         if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
+                if key == "__class__":
+                    continue
+                if key in ("created_at", "updated_at"):
                     value = datetime.fromisoformat(value)
                 if key != "__class__":
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """Returns s atring rep of the instance.
@@ -42,6 +46,7 @@ class BaseModel:
             with the current datetime.
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all values of __dict__ of instance.
